d8852e0c87c4510f3318465214b4b6f085fedbe4
Bachir Soussi Chiadmi <bachir@g-u-i.me>
2015-01-20 15:08:32 +0100

    cleaned dico theme, bowered components

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 
 $config['site_title'] = 'Le dico du spectateur';			// Site title
 $config['base_url'] = ''; 				// Override base URL (e.g. http://example.com)
-$config['theme'] = 'pico-foundation'; 			// Set the theme (defaults to "default")
+$config['theme'] = 'dico'; 			// Set the theme (defaults to "default")
 $config['date_format'] = 'jS M Y';		// Set the PHP date format
 $config['twig_config'] = array(			// Twig settings
 	'cache' => false,					// To enable Twig caching change this to CACHE_DIR
