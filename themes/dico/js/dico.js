546ba82906b61f822eb1513e520577c8a77448f5
sarah garcin <sarah@g-u-i.me>
2015-01-27 16:02:47 +0100

    add dialog pop-up ajax content load on definition page

--- themes/dico/js/dico.js
+++ themes/dico/js/dico.js
@@ -1,10 +1,28 @@
-jQuery(document).ready(function($) {
-  console.log('hello dico!');
+$(document).ready(function() {
 
+	$('.definition-list .a-item').each(function() {
+		var $link = $(this);
+		var $dialog = $('<div></div>')
+			.load($link.attr('href'))
+			.dialog({
+				autoOpen: false,
+				closeText: "x",
+				position: { my: "left top", at: "left top", of: $link },
+				width: 300
+			});
 
+		$link.click(function() {
+			$dialog.dialog('open');
 
+			return false;
+		});
 
-
+		$('body').bind('click', function(e) {
+        	if($dialog.dialog('isOpen') && !$(e.target).is('.ui-dialog, a') && !$(e.target).closest('.ui-dialog').length) {
+            	$dialog.dialog('close');
+        	}
+        });
+	});
 
 
 });
